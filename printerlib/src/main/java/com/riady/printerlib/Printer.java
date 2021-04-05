eb74bb2ca0afa84f585d8cce5b31f3912fe99513
Riady Santoso <gikiet82@gmail.com>
2021-04-05 09:06:02 +0700

    :zap: add get state

--- printerlib/src/main/java/com/riady/printerlib/Printer.java
+++ printerlib/src/main/java/com/riady/printerlib/Printer.java
@@ -1,17 +1,32 @@
 package com.riady.printerlib;
 
+import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothDevice;
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Toast;
 
 import com.riady.printerlib.Command.PrinterCommand;
 import com.riady.printerlib.Service.BluetoothService;
 
+import static android.provider.Settings.Global.DEVICE_NAME;
+import static com.riady.printerlib.Service.BluetoothService.MESSAGE_CONNECTION_LOST;
+import static com.riady.printerlib.Service.BluetoothService.MESSAGE_DEVICE_NAME;
+import static com.riady.printerlib.Service.BluetoothService.MESSAGE_READ;
+import static com.riady.printerlib.Service.BluetoothService.MESSAGE_STATE_CHANGE;
+import static com.riady.printerlib.Service.BluetoothService.MESSAGE_TOAST;
+import static com.riady.printerlib.Service.BluetoothService.MESSAGE_UNABLE_CONNECT;
+import static com.riady.printerlib.Service.BluetoothService.MESSAGE_WRITE;
+import static com.riady.printerlib.Service.BluetoothService.TOAST;
+
 public class Printer {
     private Context context;
+    private String mConnectedDeviceName = null;
 
-    public Printer(Context context,BluetoothService mService) {
+    public Printer(Context context) {
         this.context = context;
-        Common.mService = mService;
+        Common.mService = new BluetoothService(context, mHandler);
     }
 
     public void Start(){
@@ -29,6 +44,14 @@ public class Printer {
         Common.mService.connect(device);
     }
 
+    public int GetState(){
+        if (Common.mService != null) {
+            return Common.mService.getState();
+        } else {
+            return BluetoothService.STATE_NONE;
+        }
+    }
+
     public void SendDataString(String content){
         PrinterCommand.SendDataString(context, content);
     }
@@ -40,4 +63,51 @@ public class Printer {
     public void PrintPDF147Code(String content,Integer width, Integer height) {
         PrinterCommand.PrintPDF147Code(context,content,width,height);
     }
+
+    @SuppressLint("HandlerLeak")
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case BluetoothService.STATE_CONNECTED:
+                            Toast.makeText(context,"connected: " + mConnectedDeviceName,Toast.LENGTH_LONG).show();
+                            break;
+                        case BluetoothService.STATE_CONNECTING:
+                            Toast.makeText(context,"connecting ...",Toast.LENGTH_LONG).show();
+                            break;
+                        case BluetoothService.STATE_LISTEN:
+                        case BluetoothService.STATE_NONE:
+                            Toast.makeText(context,"not connected printer",Toast.LENGTH_LONG).show();
+                            break;
+                    }
+                    break;
+                case MESSAGE_WRITE:
+                    break;
+                case MESSAGE_READ:
+                    break;
+                case MESSAGE_DEVICE_NAME:
+                    // save the connected device's name
+                    mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
+                    Toast.makeText(context,
+                            "Connected to " + mConnectedDeviceName,
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                case MESSAGE_TOAST:
+                    Toast.makeText(context,
+                            msg.getData().getString(TOAST), Toast.LENGTH_SHORT)
+                            .show();
+                    break;
+                case MESSAGE_CONNECTION_LOST:
+                    Toast.makeText(context, "Device connection was lost",
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                case MESSAGE_UNABLE_CONNECT:
+                    Toast.makeText(context, "Unable to connect device",
+                            Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
 }
