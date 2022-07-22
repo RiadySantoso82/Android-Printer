49ef12fd82299b7d7a5e6a2a64b5a280aa0066ce
Riady Santoso <gikiet82@gmail.com>
2022-07-22 16:51:13 +0700

    :bug: try to fix bug thread

--- printerlib/src/main/java/com/riady/printerlib/Service/BluetoothService.java
+++ printerlib/src/main/java/com/riady/printerlib/Service/BluetoothService.java
@@ -128,20 +128,24 @@ public class BluetoothService implements Thread.UncaughtExceptionHandler {
      * @param device  The BluetoothDevice to connect
      */
     public synchronized void connect(BluetoothDevice device) {
-        if (DEBUG) Log.d(TAG, "connect to: " + device);
+        if (device != null) {
+            if (DEBUG) Log.d(TAG, "connect to: " + device);
 
-        // Cancel any thread attempting to make a connection
-        if (mState == STATE_CONNECTING) {
-            if (mConnectThread != null) {mConnectThread.cancel(); mConnectThread = null;}
+            // Cancel any thread attempting to make a connection
+            if (mState == STATE_CONNECTING) {
+                if (mConnectThread != null) {mConnectThread.cancel(); mConnectThread = null;}
+            }
+
+            // Cancel any thread currently running a connection
+            if (mConnectedThread != null) {mConnectedThread.cancel(); mConnectedThread = null;}
+
+            // Start the thread to connect with the given device
+            mConnectThread = new ConnectThread(device);
+            mConnectThread.start();
+            setState(STATE_CONNECTING);
+        } else {
+            connectionFailed();
         }
-
-        // Cancel any thread currently running a connection
-        if (mConnectedThread != null) {mConnectedThread.cancel(); mConnectedThread = null;}
-
-        // Start the thread to connect with the given device
-        mConnectThread = new ConnectThread(device);
-        mConnectThread.start();
-        setState(STATE_CONNECTING);
 
     }
 
